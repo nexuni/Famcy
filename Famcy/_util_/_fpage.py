e756132be8e36de0cefd8d41617df30157307244
Weitung Chen <weitung@mit.edu>
2021-10-25 09:22:40 -0400

    add background thread function

--- Famcy/_util_/_fpage.py
+++ Famcy/_util_/_fpage.py
@@ -3,6 +3,8 @@ from Famcy._util_._flayout import *
 from Famcy._util_._fsubmission import *
 from flask import g
 import Famcy
+import time
+import abc
 
 class FPage(FamcyWidget):
 	"""
@@ -31,13 +33,29 @@ class FPage(FamcyWidget):
 		* postload(): actions after the rendering
 	"""	
 	def __init__(self, route, style, permission_level=0, 
-			layout_mode=FamcyLayoutMode.recommend):
+			layout_mode=FamcyLayoutMode.recommend, 
+			background_thread=False, background_freq=0.5, 
+			comet_update_freq=0.25):
 
 		super(FPage, self).__init__(permission_level)
 		self.route = route
 		self.style = style
-
 		self.layout = FamcyLayout(self, layout_mode)
+		self.background_thread_flag = background_thread
+
+		if self.background_thread_flag:
+			self.comet_update_freq = comet_update_freq
+			self.background_freq = background_freq
+			self.background_signal = FamcySignal()
+			self.sijax_response = None
+			# Necessary header script for comet
+			self.header_script += '<script type="text/javascript" src="/static/js/sijax/sijax_comet.js"></script>'
+			
+			# Check loop correctness
+			assert getattr(self, "background_thread_inner", None), "Must implement background_thread_inner"
+			self.bthread = FamcyThread(target=self.background_thread_loop, daemon=True)
+			self.bthread.start()
+
 		self._check_rep()
 
 	def _check_rep(self):
@@ -68,13 +86,54 @@ class FPage(FamcyWidget):
 		# First setup the submission handler
 		if g.sijax.is_sijax_request:
 			g.sijax.register_object(FSubmissionSijaxHandler)
+
+			if self.background_thread_flag:
+				g.sijax.register_comet_callback('background_work', self.background_main_comet_handler)
+
 			return g.sijax.process_request()
 
 		# Render all content
 		content_data = super(FPage, self).render()
 
 		# Apply style at the end
-		return self.style.render(self.header_script, content_data)
+		return self.style.render(self.header_script, content_data, background_flag=self.background_thread_flag)
+
+	def background_thread_loop(self):
+		"""
+		This is the background thread 
+		loop for fpage
+		"""
+		while True:
+			time.sleep(int(1/self.background_freq))
+
+			if not self.sijax_response:
+				continue
+
+			self.background_thread_inner()
+			
+
+	def background_thread_inner(self, sijax_response):
+		"""
+		This is the background thread
+		inner content for fpage. 
+		* Input:
+			sijax_response: the object that can
+			help trigger sijax response
+		"""
+		pass
+
+	def background_main_comet_handler(self, obj_response):
+		"""
+		This is the main handler
+		for sijax comet plugin
+		"""
+		while True:
+			self.sijax_response = obj_response
+			self.background_signal.emit()
+
+			yield self.sijax_response
+
+			time.sleep(int(1/self.comet_update_freq))
 
 	# Functions that can be overwritten
 	# ---------------------------------
