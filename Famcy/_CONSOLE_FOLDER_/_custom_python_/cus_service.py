837fb67b50a195e103290a1696043a294f7fdda3
Weitung Chen <weitung@mit.edu>
2021-10-25 23:06:12 -0400

    login half way

--- Famcy/_CONSOLE_FOLDER_/_custom_python_/cus_service.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-import urllib
-import Famcy
-from flask import Flask, g, render_template, redirect, url_for, session, flash, request, Blueprint, current_app, render_template_string, Response
-
-cus = Blueprint('cus', __name__)
--- Famcy/_responses_/RedirectPage/RedirectPage.py
+++ Famcy/_responses_/RedirectPage/RedirectPage.py
@@ -9,4 +9,4 @@ class RedirectPage(Famcy.FamcyResponse):
 	def response(self, sijax_response):
 		if "redirect_url" in self.info_dict.keys():
 			self.redirect_url = self.info_dict["redirect_url"]
-			sijax_response.redirect(url_for(self.redirect_url))
+			sijax_response.redirect(self.redirect_url)
