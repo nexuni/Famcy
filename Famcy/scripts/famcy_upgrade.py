f89d101592de7e3bc4924668cb4cb3322206100f
Weitung Chen <weitung@mit.edu>
2021-10-27 12:30:44 -0400

    update famcy

--- Famcy/scripts/famcy_upgrade.py
+++ Famcy/scripts/famcy_upgrade.py
@@ -1,9 +1,9 @@
 import subprocess
 import pkg_resources
-from Famcy import FManager
+import Famcy
 
 def main(args):
 	famcy_id = args[0]
-	script_output = subprocess.check_output(["bash", FManager.main+"/scripts/bash/"+"upgrade.sh", FManager.main, famcy_id]) 
+	script_output = subprocess.check_output(["bash", Famcy.famcy_dir+"/scripts/bash/"+"upgrade.sh", Famcy.famcy_dir, famcy_id]) 
 	print("[Famcy Upgrade] ", script_output.decode())
 	print("Famcy version after upgrade: ", pkg_resources.require("Famcy")[0].version)
--- Famcy/scripts/start.py
+++ Famcy/scripts/start.py
@@ -1,8 +1,8 @@
 import subprocess
-from Famcy import FManager
+import Famcy
 
 def main(args):
 	famcy_id = args[0]
 	if "--dev" in args:
 		_ = subprocess.check_output(["export", "FLASK_ENV=development"]) 
-	_ = subprocess.check_output(["bash", FManager.main+"/scripts/bash/"+"run.sh", FManager.main, famcy_id]) 
+	_ = subprocess.check_output(["bash", Famcy.famcy_dir+"/scripts/bash/"+"run.sh", Famcy.famcy_dir, famcy_id]) 
