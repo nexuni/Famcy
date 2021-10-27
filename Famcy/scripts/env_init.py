164075dc90926c1e8d67effe4a03ace813f8be0e
Weitung Chen <weitung@mit.edu>
2021-10-27 12:30:44 -0400

    update famcy

--- Famcy/scripts/env_init.py
+++ Famcy/scripts/env_init.py
@@ -1,7 +1,7 @@
 import subprocess
-from Famcy import FManager
+import Famcy
 
 def main(args):
 	famcy_id = args[0]
-	init_script_output = subprocess.check_output(["bash", FManager.main+"/scripts/bash/"+"init.sh", famcy_id]) 
+	init_script_output = subprocess.check_output(["bash", Famcy.famcy_dir+"/scripts/bash/"+"init.sh", famcy_id]) 
 	print("[Famcy Init] ", init_script_output.decode())
