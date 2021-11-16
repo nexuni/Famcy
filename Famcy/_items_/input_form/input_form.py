d979c4c4f1c5a99f33ce971aad7b49950755b82d
Julia <weichingchen0703@gmail.com>
2021-11-17 04:30:37 +0800

    update parent card

--- Famcy/_items_/input_form/input_form.py
+++ Famcy/_items_/input_form/input_form.py
@@ -20,6 +20,10 @@ class input_form(Famcy.FamcyCard):
 		self.body["onsubmit"] = "return false;"
 
 	def render_inner(self):
+		parent_card = self.parent_card if self.parent_card else self
+		for item, _, _, _, _ in self.layout.content:
+			item.parent_card = parent_card
+			
 		header_script, content_render = self.layout.render()
 		if header_script not in self.header_script:
 			self.header_script += header_script
