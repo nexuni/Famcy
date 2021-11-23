cc5ca1f4918acc3d89851fc38ed79f584fdb0566
Julia <weichingchen0703@gmail.com>
2021-11-24 06:51:24 +0800

    famcy fetch loop sijax

--- Famcy/static/js/background_loop.js
+++ Famcy/static/js/background_loop.js
@@ -1,11 +1,22 @@
-async function background_loop(url) {
+async function background_loop(url, page_id) {
 	console.log("background_loop!!!!!!!!!!!!!!!!!")
     
     // Storing response
     const response = await fetch(url);
-    const text = await response.json();
+    const res_dict = await response.json();
 
-    console.log(text)
+    console.log(res_dict)
+
+    if (res_dict["indicator"]) {
+    	var token = document.head.querySelector("[name~=csrf-token][content]").content
+    	var data = res_dict["message"]
+
+		console.log(data, data.page_id)
+		
+		if (data.page_id  === page_id) {
+			Sijax.request('famcy_submission_handler', [data.submission_id, data.data], { data: { csrf_token: token } });
+		}
+    }
     
     // Storing data in form of JSON
     // var data = await response.json();
