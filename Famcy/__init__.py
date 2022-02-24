184e558a6eb46c13ed3a2dc65c60e5410f3bc6ec
Julia <weichingchen0703@gmail.com>
2022-02-24 09:56:50 +0800

    lg func, video stream, list

--- Famcy/__init__.py
+++ Famcy/__init__.py
@@ -64,66 +64,11 @@ FamcyStyleNavBar = FStyleNavBar
 FamcyStyleNavBtns = FStyleNavBtns
 FamcyBackgroundTask = FBackgroundTask
 
-import rclpy
-import signal
-from rclpy.node import Node
-from std_msgs.msg import String
-import threading
-
-class TestPublisher(Node):
-    def __init__(self):
-        super().__init__('test_publisher')
-        self.publisher = self.create_publisher(String, 'flask_pub_topic', 10)
-        self.subscription = self.create_subscription(
-            String,
-            '/topic',
-            self.chatter_callback,
-            10)
-        self.latest_message = None
-
-    def chatter_callback(self, msg):
-        print(f'chatter cb received: {msg.data}')
-        self.latest_message = msg.data
-
-    def publish_message(self):
-        msg = String()
-        msg.data = 'hello, world!'
-        self.publisher.publish(msg)
-
-
-def ros2_thread(node):
-    print('entering ros2 thread')
-    rclpy.spin(node)
-    print('leaving ros2 thread')
-
-
-def sigint_handler(signal, frame):
-    """
-    SIGINT handler
-
-    We have to know when to tell rclpy to shut down, because
-    it's in a child thread which would stall the main thread
-    shutdown sequence. So we use this handler to call
-    rclpy.shutdown() and then call the previously-installed
-    SIGINT handler for Flask
-    """
-    rclpy.shutdown()
-    if prev_sigint_handler is not None:
-        prev_sigint_handler(signal)
-
-
-
 def create_app(famcy_id, production=False):
 	"""
 	Main creation function of the famcy application. 
 	Can set to different factory settings in the future. 
 	"""
-
-	rclpy.init(args=None)
-	ros2_node = TestPublisher()
-	threading.Thread(target=ros2_thread, args=[ros2_node]).start()
-	prev_sigint_handler = signal.signal(signal.SIGINT, sigint_handler)
-
 	# Famcy Manager that manage all global vars, imports, 
 	# file systems, http
 	FManager = FamcyManager(famcy_id, famcy_dir, production=production)
@@ -172,6 +117,7 @@ def create_app(famcy_id, production=False):
 	# Security Enhance
 	FManager.register_csrf(app)
 
+	# redis server
 	store = RedisStore(redis.StrictRedis())
 	globals()["store"] = store
 	if "serialization_method" in FManager["ConsoleConfig"].keys() and FManager["ConsoleConfig"]["serialization_method"] == "dill":
@@ -179,6 +125,9 @@ def create_app(famcy_id, production=False):
 	else:
 		KVSessionInterface.serialization_method = pickle
 	KVSessionExtension(store, app)
+
+	# ros2
+	FManager.ros2_init()
 
 	# User Static Data
 	@MainBlueprint.route('/asset/<path:filename>')
