f5fd7add8b881166de6e45fdac1c3a17db69a39d
Julia <weichingchen0703@gmail.com>
2022-04-26 22:17:04 +0800

    update apistyle

--- Famcy/_util_/_fpage.py
+++ Famcy/_util_/_fpage.py
@@ -128,26 +128,22 @@ class FPage(FamcyWidget):
 			else:
 				Famcy.FManager["BgBlueprint"].route(cls.route+"/bgloop")(bg_func)
 
+	@staticmethod
+	def get_request_args(key_name):
+		return request.args.get(key_name)
+
 	@classmethod
 	def render(cls, init_cls=None, *args, **kwargs):
 
-		# handle race condition issue: lock the function
-		#cls._lock.acquire()
-
-		# try:
 		route_list = request.path[1:].split("/")
 		route_name = '_'.join(route_list)
 
-		# print("cls.style._sijax_enable: ", cls.style, cls.style._sijax_enable)
 		if cls.style._sijax_enable and request.method == 'POST' and g.sijax.is_sijax_request:
 			## Only POST and sijax enable request can enter here
-			# print("g.sijax.is_sijax_request")
 			FSubmissionSijaxHandler.current_page = session.get(route_name+'current_page')
 
 			g.sijax.register_object(FSubmissionSijaxHandler)
 
-			# print("SIJAX session ========", session)
-			# print("session_key: ", route_name+'current_page')
 			# code for upload form
 			upload_list = session.get(route_name+'current_page').find_class(session.get(route_name+'current_page'), "upload_form")
 			for _item in upload_list:
@@ -156,15 +152,10 @@ class FPage(FamcyWidget):
 
 			sijax_res = g.sijax.process_request()
 
-			# handle race condition issue: unlock the function to allow the next request
-			#cls._lock.release()
-
 			return sijax_res
 
-		# print("Non SIJAX ========", session)
 		# init page
 		if request.method == 'GET':
-			# print("GET render")
 			if init_cls:
 				current_page = init_cls
 			else:
@@ -173,33 +164,24 @@ class FPage(FamcyWidget):
 						del session[route_name+"BackgroundQueueDict"]
 					del session[route_name+"current_page"]
 
-				# cls._lock.acquire()
-
 				# reset Famcy widget id
 				FamcyWidget.reset_id()
 				current_page = cls()
-
-				# #cls._lock.release()
 
 			if not isinstance(cls.style, Famcy.VideoStreamStyle):
 				print(route_name + "___ Save to session current_page")
 				session[route_name+"current_page"] = current_page
 
 		elif request.method == 'POST':
-			# print("POST render *************")
-			#cls._lock.release()
 			if session[route_name+"current_page"]:
 				current_page = session[route_name+"current_page"]
 			else:
 				print("POST didn't receive anything *************")
 				return ""
-				
-		#cls._lock.release()
 
 		form_init_js = ''	# no use
 		end_script = ''
 		if not current_page.permission.verify(Famcy.FManager["CurrentUser"]):
-			# content_data = "<h1>You are not authorized to view this page!</h1>"
 			session["login_permission"] = "You are not authorized to view this page!"
 
 			return redirect(url_for("MainBlueprint.famcy_route_func_name_"+Famcy.FManager["ConsoleConfig"]['login_url'].replace("/", "_")))
@@ -216,19 +198,10 @@ class FPage(FamcyWidget):
 
 			# Apply style at the end
 			return current_page.style.render(current_page.header_script+head_script, content_data, event_source_flag=current_page.event_source_flag, background_flag=current_page.background_thread_flag, route=current_page.route, time=int(1/current_page.background_freq)*1000, form_init_js=form_init_js, end_script=end_script)
-		# except Exception as e:
-			# unlock the function while getting error
-			#cls._lock.release()
-			# print(e)
-		# print("FALLBACK")
 		return ""
 
 	@staticmethod
 	def background_generator_loop():
-		# handle race condition issue: lock the function
-		# cls._lock.acquire()
-
-		# try:
 		route_list = request.path[1:].split("/")
 		del route_list[-1]
 		route_name = '_'.join(route_list)
@@ -249,16 +222,8 @@ class FPage(FamcyWidget):
 		def generate():
 			yield json.dumps({"indicator": indicator, "message": message})
 
-		# handle race condition issue: unlock the function to allow the next request
-		# #cls._lock.release()
-
+		
 		return Response(generate(), mimetype='text/plain')
-
-		# except:
-		# handle race condition issue: unlock the function to allow the next request
-		print("bg FALLBACK")
-		# #cls._lock.release()
-		return ""
 
 	def background_thread_inner(self):
 		"""
